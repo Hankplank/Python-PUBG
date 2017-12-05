073f52ea89ecdb888f5546ecfda8cfacd2334b4a
iwonhitwonder <iwonhitwonder@gmail.com>
2017-12-04 21:29:40 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PUBG API Wrapper',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/iwonhitwonder/Python-PUBG',
+    license='',
+    author='Henry Brady',
+    author_email='hbradydevelopment@gmail.com',
+    description='Python wrapper for the PUBG API'
+)
